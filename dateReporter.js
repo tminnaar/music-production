d7dc8d8fec45d5c9025598410565b5cf2a9ea13a
tminnaar <tminnaar94@gmail.com>
2022-11-24 22:40:37 +0000

    js comments

--- dateReporter.js
+++ dateReporter.js
@@ -1,8 +1,10 @@
 const dateElement = document.getElementById("dateText");
 
+//methods to grab the date, month and year.
 let today = new Date();
 let day = today.getDate();
 let month = today.getMonth() + 1;
 let year = today.getFullYear();
 
+//string interpolates the values into the dateText element.
 dateElement.textContent = `Today's date is ${day}/${month}/${year}`;
